4df12c381a227aaf12b688cdc6483aa6cbff71b0
Panza <gabrielpanzeri@live.com>
2020-09-28 15:58:51 -0300

    added list method

--- index.php
+++ index.php
@@ -2,8 +2,28 @@
 
 require_once("config.php");
 
-$usuario = new Usuario();
+// carrega um usuario
+// $usuario = new Usuario();
+// $usuario->loadById(4);
 
-$usuario->loadById(4);
+// echo $usuario;
+
+
+
+// carrega uma lista de usuários
+// $list = Usuario::getList();
+// echo json_encode($list);
+
+
+// carrega uma lista de usuarios buscando pelo login
+// $search = Usuario::search("an");
+
+// echo json_encode($search);
+
+
+
+// carrega um usuário usando login e senha
+$usuario = new Usuario();
+$usuario->login("analfa", "fut");
 
 echo $usuario;
